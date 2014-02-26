fa521f893a39a6330286e4fea074bc620879edfc
Georg Konwisser <gekonwi@gmail.com>
2014-02-25 22:57:16 -0500

    REFACTORED: reduced complexity in triangle area calculation

--- eclipse_deliverable11.1/src/triangle.rb
+++ eclipse_deliverable11.1/src/triangle.rb
@@ -8,6 +8,9 @@ class Triangle
 	end
 
 	def area
-		((p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y)) / 2.0).abs
+		dividend = p1.x * (p2.y - p3.y)
+		dividend += p2.x * (p3.y - p1.y)
+		dividend += p3.x * (p1.y - p2.y)
+		(dividend / 2.0).abs
 	end
 end
